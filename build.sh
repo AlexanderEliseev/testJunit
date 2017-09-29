f1b98ae1c8a2acc106c3b6a589d67a82fe715d4d
alexander.eliseev <alexander.eliseev@graph.uk>
2017-09-29 12:23:50 +0500

    Make test results by copying example file

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# remove old report if exist
+if [ -f ./test-results.xml ]; then
+    rm -rf ./test-results.xml
+fi
+
+#"make" new test results
+cp ./reportExample.xml ./test-results.xml
